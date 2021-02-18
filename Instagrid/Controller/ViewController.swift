5acfc323b9c901683b279da85b1d2d8b34176984
farid benjomaa <farid.benjomaa@hotmail.fr>
2021-02-18 12:35:27 +0100

    ajout des IBouletCollection

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var LayoutChoice: [UIButton]!
+    @IBOutlet var layoutVue: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
