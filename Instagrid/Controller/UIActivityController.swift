56bb34c06d13796885155e160a6f66834c8a57e5
farid benjomaa <farid.benjomaa@hotmail.fr>
2021-02-18 12:25:46 +0100

    Ajout des dossier Model vue et contrôler

---
+++ Instagrid/Controller/UIActivityController.swift
@@ -0,0 +1,13 @@
+//
+//  UIActivityController.swift
+//  Instagrid
+//
+//  Created by Farid Benjomaa on 18/02/2021.
+//
+
+import Foundation
+
+class UIActivityController: UIActivityController {
+    
+    
+}
---
+++ Instagrid/Model/Layout.swift
@@ -0,0 +1,15 @@
+//
+//  Layout.swift
+//  Instagrid
+//
+//  Created by Farid Benjomaa on 18/02/2021.
+//
+
+import Foundation
+
+class Layout {
+    
+    var name : String = ""
+    
+    
+}
